3e1494dc3860126d17236121790b55ebd26b3298
Contea <Contea95@naver.com>
2020-07-07 16:00:43 +0900

    firebase

--- CoronaCheck/app/src/main/java/kr/ac/kpu/coronacheck/MainActivity.kt
+++ CoronaCheck/app/src/main/java/kr/ac/kpu/coronacheck/MainActivity.kt
@@ -3,6 +3,7 @@ package kr.ac.kpu.coronacheck
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
