a125a83888cb24c09a6fc53e55bdbb246489f569
choi3179 <jacobis@naver.com>
2020-07-07 14:16:07 +0900

    ㅇㅇ

--- CoronaCheck/app/src/main/java/kr/ac/kpu/coronacheck/MainActivity.kt
+++ CoronaCheck/app/src/main/java/kr/ac/kpu/coronacheck/MainActivity.kt
@@ -1,12 +1,19 @@
 package kr.ac.kpu.coronacheck
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnlogin.setOnClickListener(){
+            val intent= Intent(this,CheckList.class)
+            startActivity(intent)
+        }
 
     }
 }
