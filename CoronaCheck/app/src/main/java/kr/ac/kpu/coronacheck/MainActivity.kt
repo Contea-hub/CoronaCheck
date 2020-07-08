179beabb91387d981e3aa61386d567fa370a5eec
Contea <Contea95@naver.com>
2020-07-08 16:10:38 +0900

    Can Add Firebase DB
    로그캣에 han1로 치면 알 수 있다.

--- CoronaCheck/app/src/main/java/kr/ac/kpu/coronacheck/MainActivity.kt
+++ CoronaCheck/app/src/main/java/kr/ac/kpu/coronacheck/MainActivity.kt
@@ -4,13 +4,23 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private val RC_SIGN_IN = 9001       //google login result
     private var firebaseAuth: FirebaseAuth? = null      //firebase auth
+    //var firestore : FirebaseFirestore? = FirebaseFirestore.getInstance()           //firestroe 선언
+    private lateinit var database : DatabaseReference
+    private var mDatabase : DatabaseReference? = null
+    private var mMessageReference : DatabaseReference? =null
     data class UserInfo(var stunum : String? = null, var temp : String? =null, var q1 : Boolean? = null, var q2 : Boolean? =null,
     var q3 : Boolean? = null, var q4 : Boolean? =null)      //DB 유저 정보 저장 유형
     private fun createEmail(){
@@ -52,15 +62,16 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        var stunum : String?
+
+        val stunum : String?
         var q1 : Boolean?
         var q2 : Boolean?
         var q3 : Boolean?
         var q4 : Boolean?
-        var temp : String?
+        val temp : String?
+
         if(resultCode == Activity.RESULT_OK){
             stunum = data!!.getStringExtra("stunum")
             q1 = data!!.getBooleanExtra("q1", true)
@@ -68,15 +79,32 @@ class MainActivity : AppCompatActivity() {
             q3 = data!!.getBooleanExtra("q3", true)
             q4 = data!!.getBooleanExtra("q4", true)
             temp = data!!.getStringExtra("temp")
-            Toast.makeText(this, "${stunum}, ${q1}, ${q2},${q3},${q4},${temp}", Toast.LENGTH_SHORT).show()
+            //Toast.makeText(this, "${stunum}, ${q1}, ${q2},${q3},${q4},${temp}", Toast.LENGTH_SHORT).show()
+            var userInfo = UserInfo(stunum, temp, q1, q2, q3, q4)
+            database = Firebase.database.reference
+            database.child("users").child(stunum).setValue(userInfo)
+            val postListener = object : ValueEventListener{
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    val post = dataSnapshot.getValue()
+                    Log.d("han1", "$post")
+                }
+
+                override fun onCancelled(p0: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+            }
+            mMessageReference!!.addValueEventListener(postListener)
+
         }
     }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        firebaseAuth = FirebaseAuth.getInstance()
-
+        firebaseAuth = FirebaseAuth.getInstance()//
+        mDatabase = FirebaseDatabase.getInstance().reference
+        mMessageReference = FirebaseDatabase.getInstance().getReference("users")
         btnlogin.setOnClickListener{        //로그인 버튼
             loginEmail()
         }
