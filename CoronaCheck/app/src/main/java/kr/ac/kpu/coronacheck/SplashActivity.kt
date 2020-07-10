468d6b89f95e9b4581159ec4bfd99cc41a239c32
choi3179 <jacobis@naver.com>
2020-07-10 14:15:53 +0900

    스플래쉬 추가

--- CoronaCheck/app/src/main/java/kr/ac/kpu/coronacheck/SplashActivity.kt
+++ CoronaCheck/app/src/main/java/kr/ac/kpu/coronacheck/SplashActivity.kt
@@ -3,13 +3,18 @@ package kr.ac.kpu.coronacheck
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val SPLASH_VIEW_TIME:Long=100
         val intent= Intent(this,MainActivity::class.java)
-        startActivity(intent)
-        finish()
+        Handler().postDelayed({
+            startActivity(intent)
+            finish()
+        },SPLASH_VIEW_TIME)
+
     }
 }
