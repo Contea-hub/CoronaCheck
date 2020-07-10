cd25ca4c3e23f9140c23704a60e9e1c2d203af24
Smart-mj <pds514514@naver.com>
2020-07-10 15:21:28 +0900

    Update ListStudent.kt

--- CoronaCheck/app/src/main/java/kr/ac/kpu/coronacheck/ListStudent.kt
+++ CoronaCheck/app/src/main/java/kr/ac/kpu/coronacheck/ListStudent.kt
@@ -10,15 +10,32 @@ import kotlinx.android.synthetic.main.activity_list_student.*
 
 class ListStudent : AppCompatActivity() {
     var studentList= arrayListOf<student>(
-        student("박다수","2016150016","firebase",true),
-    student("정지운","2016150036","firebase",true),
-    student("응옌뒤홍","2016150041","iOS",true)
     )
+    var mDatabase: FirebaseDatabase? = null
+    var mReference: DatabaseReference? = null
+    var mChild: ChildEventListener? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_student)
+        initDatabase()
+
+        mReference = mDatabase!!.getReference("child 이름") // 변경값을 확인할 child 이름
+
+        mReference!!.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                for (messageData in dataSnapshot.children) {
+                    // child 내에 있는 데이터만큼 반복합니다.
+                    val msg2 = messageData.value
+                    studentList.add(msg2 as student)
+                }
+                (studentRV.adapter as adapterStudent).notifyDataSetChanged()
+            }
+            override fun onCancelled(databaseError: DatabaseError){
+            }
+
+        })
 
         var data:Any
         var bundle:Bundle
@@ -31,8 +48,29 @@ class ListStudent : AppCompatActivity() {
         studentRV.layoutManager=lm
         studentRV.setHasFixedSize(true)
 
-
-
     }
 
-}
+
+    fun initDatabase() {
+
+        mDatabase = FirebaseDatabase.getInstance()
+        mReference = mDatabase!!.getReference("log")
+        mReference!!.child("log").setValue("check")
+
+        mChild = object : ChildEventListener {
+            override fun onChildAdded(dataSnapshot: DataSnapshot, s: String?) {}
+            override fun onChildChanged(dataSnapshot: DataSnapshot, s: String?) {}
+            override fun onChildRemoved(dataSnapshot: DataSnapshot) {}
+            override fun onChildMoved(dataSnapshot: DataSnapshot, s: String?) {}
+            override fun onCancelled(databaseError: DatabaseError) {}
+        }
+        mReference!!.addChildEventListener(mChild as ChildEventListener)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        this!!.mChild?.let { mReference?.removeEventListener(it) }
+    }
+
+} // 메인
+
