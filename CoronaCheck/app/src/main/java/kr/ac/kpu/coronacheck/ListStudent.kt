55a5168fafc7d0608b82ce76642b2a5bad6e95f8
Contea <Contea95@naver.com>
2020-07-10 14:19:32 +0900

    refresh

--- CoronaCheck/app/src/main/java/kr/ac/kpu/coronacheck/ListStudent.kt
+++ CoronaCheck/app/src/main/java/kr/ac/kpu/coronacheck/ListStudent.kt
@@ -3,11 +3,17 @@ package kr.ac.kpu.coronacheck
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.AdapterView
 import android.widget.ListView
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_list_student.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class ListStudent : AppCompatActivity() {
     var studentList= arrayListOf<student>(
@@ -16,13 +22,52 @@ class ListStudent : AppCompatActivity() {
     student("응옌뒤홍","2016150041","iOS",true)
     )
 
+    private lateinit var database : DatabaseReference
+    private var mDatabase : DatabaseReference? = null
+    private var mMessageReference : DatabaseReference? =null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_student)
+        mDatabase = FirebaseDatabase.getInstance().reference
+        mMessageReference = FirebaseDatabase.getInstance().getReference()
+        database = Firebase.database.reference
+        database.child("change").child("change").setValue("0")
 
-        var data:Any
-        var bundle:Bundle
-        var intent: Intent
+
+        val postListener = object : ValueEventListener{
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                studentList.add(student(dataSnapshot.child("555555").child("name").value.toString(),
+                    dataSnapshot.child("555555").child("stunum").value.toString(),
+                    dataSnapshot.child("555555").child("subsel").value.toString(),
+                    true
+                ))
+                studentList.add(student(dataSnapshot.child("555555").child("name").value.toString(),
+                    dataSnapshot.child("555555").child("stunum").value.toString(),
+                    dataSnapshot.child("555555").child("subsel").value.toString(),
+                    true
+                ))
+                studentList.add(student(dataSnapshot.child("555555").child("name").value.toString(),
+                    dataSnapshot.child("555555").child("stunum").value.toString(),
+                    dataSnapshot.child("555555").child("subsel").value.toString(),
+                    true
+                ))
+
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        }
+        mMessageReference!!.addValueEventListener(postListener)
+
+        refreshButton.setOnClickListener {
+            val mAdapter=adapterStudent(this,studentList)
+            studentRV.adapter=mAdapter
+            val lm=LinearLayoutManager(this)
+            studentRV.layoutManager=lm
+            studentRV.setHasFixedSize(true)
+        }
 
         val mAdapter=adapterStudent(this,studentList)
         studentRV.adapter=mAdapter
